156082f6f6afea739041f772674eb086f5b0f249
Denys Yahofarov <denyago@gmail.com>
2022-06-05 23:58:03 +0200

    Automate creation of releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     application
     kotlin("jvm") version "1.6.21"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "name.denyago"
